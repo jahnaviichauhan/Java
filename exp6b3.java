46ea0c1be95b05eb991c785ff502031bc8dde308
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2023-10-28 15:27:27 +0530

    Add files via upload

---
+++ exp6b3.java
@@ -0,0 +1,19 @@
+//WAP in Java to create a package
+//Author: Jahnavi Singh Chauhan
+//Roll No.: R2142220526
+//SAP ID: 500102342
+//Date: 30/09/2023
+
+// Import the mypack package to access and call the function "void disp1()"
+package otherpack;
+
+// Import the mypack package to access and call the function "void disp1()"
+import mypack.exp6b1;
+
+public class exp6b3 {
+    public static void main(String[] args) {
+        // Call the function from simple2.java using package import
+        exp6b1.disp1();
+    }
+}
+
