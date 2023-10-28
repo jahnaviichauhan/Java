e4874a4bb3bfd97651d58ffa913898c7a3ff71ae
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2023-10-28 15:27:09 +0530

    Add files via upload

---
+++ exp6b2.java
@@ -0,0 +1,13 @@
+//WAP in Java to create a package
+//Author: Jahnavi Singh Chauhan
+//Roll No.: R2142220526
+//SAP ID: 500102342
+//Date: 30/09/2023
+
+package mypack;
+public class exp6b2 {
+    public static void main(String[] args){
+        exp6b1.disp1();
+    }
+}
+
